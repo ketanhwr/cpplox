dfb98fded8785be19ec3938a6b836f2c3858f13c
Ketan Gupta <ketan19972010@gmail.com>
2024-06-08 16:19:23 +0530

    Visitor pattern for AST traversal

---
+++ src/prettyprinter.cpp
@@ -0,0 +1,82 @@
+#include "../build/expr.hpp"
+
+#include <iostream>
+#include <sstream>
+
+struct AstPrinter: public AbstractVisitor
+{
+    std::ostringstream result_;
+
+    void visitBinaryExpr(BinaryExpr &exp) override
+    {
+        std::ostringstream tempResult;
+
+        tempResult << "(" << exp.op_.lexeme_ << " ";
+        
+        exp.left_.accept(*this);
+
+        tempResult << result_.str() << " ";
+
+        exp.right_.accept(*this);
+
+        tempResult << result_.str() << ")";
+
+        result_ = std::move(tempResult);
+    }
+
+	void visitGroupingExpr(GroupingExpr& expr) override
+    {
+        std::ostringstream tempResult;
+
+        tempResult << "(";
+
+        expr.expression_.accept(*this);
+
+        tempResult << result_.str() << ")";
+
+        result_ = std::move(tempResult);
+    }
+
+	void visitLiteralExpr(LiteralExpr& expr) override
+    {
+        std::ostringstream tempResult;
+
+        tempResult << expr.value_;
+        
+        result_ = std::move(tempResult);
+    }
+
+	void visitUnaryExpr(UnaryExpr& expr) override
+    {
+        std::ostringstream tempResult;
+
+        tempResult << "(" << expr.op_.lexeme_ << " ";
+
+        expr.right_.accept(*this);
+
+        tempResult << result_.str() << ")";
+
+        result_ = std::move(tempResult);
+    }
+};
+
+int main()
+{
+    Expr* expr = new BinaryExpr(
+            *(new UnaryExpr(
+                *(new Token(TokenType::MINUS, nullptr, "-", 1)),
+                *(new LiteralExpr(*(new LoxInteger(45)))))),
+            *(new Token(TokenType::STAR, nullptr, "*", 1)),
+            *(new GroupingExpr(
+                *(new LiteralExpr(*(new LoxFloat(5.5))))
+                ))
+        );
+
+    AstPrinter printer;
+
+    expr->accept(printer);
+    std::cout << printer.result_.str() << std::endl;
+    
+    return 0;
+}
+
--- tools/generate_ast.py
+++ tools/generate_ast.py
@@ -7,20 +7,33 @@ def define_ast(output_dir, base_class, ast_types):
         output_file.write("#pragma once\n\n")
         output_file.write("#include \"token.hpp\"\n\n")
 
+        # Forward declare AbstractVisitor
+        output_file.write("struct AbstractVisitor;\n\n")
+
         output_file.write(f"struct {base_class}\n")
         output_file.write("{\n")
-        output_file.write(f"\tvirtual ~{base_class}() = 0;\n")
+        output_file.write("\tvirtual void accept(AbstractVisitor& visitor) = 0;\n")
+        output_file.write(f"\tvirtual ~{base_class}() = default;\n")
         output_file.write("};\n\n")
 
         for k in ast_types:
-            output_file.write(f"struct {k};\n");
+            output_file.write(f"struct {k}{base_class};\n");
 
         output_file.write("\n");
+
+        # AbstractVisitor structure
+        output_file.write("struct AbstractVisitor\n")
+        output_file.write("{\n")
+
+        for k in ast_types:
+            output_file.write(f"\tvirtual void visit{k}{base_class}({k}{base_class}& {str.lower(base_class)}) = 0;\n")
+
+        output_file.write("};\n\n")
 
         for k, v in ast_types.items():
             attrs = [[x.strip().split()[0].strip(), x.strip().split()[1].strip()] for x in v.split('|') ]
 
-            output_file.write(f"struct {k} : public {base_class}\n")
+            output_file.write(f"struct {k}{base_class}: public {base_class}\n")
             output_file.write("{\n")
 
             for var in attrs:
@@ -32,9 +45,14 @@ def define_ast(output_dir, base_class, ast_types):
             c_args = ", ".join([f"{x[0]}& {x[1]}" for x in attrs])
             init_args = "\t\t, ".join([f"{x[1]}_{{{x[1]}}}\n" for x in attrs])
 
-            output_file.write(f"\t{k}({c_args})\n")
+            output_file.write(f"\t{k}{base_class}({c_args})\n")
             output_file.write(f"\t\t: {init_args}")
-            output_file.write("\t{ }\n")
+            output_file.write("\t{ }\n\n")
+
+            # Override accept() function
+            output_file.write("\tvoid accept(AbstractVisitor& visitor) override {\n")
+            output_file.write(f"\t\tvisitor.visit{k}{base_class}(*this);\n")
+            output_file.write("\t}\n")
 
             output_file.write("};\n\n")
 
